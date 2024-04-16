61c2c0a0ab78a93e58f68c12d16f2cd2af96e9f0
JuanSe320 <63017408+JuanSe320@users.noreply.github.com>
2024-04-16 15:35:48 +0000

    Actualizacion de fork

---
+++ Assets/Scripts/CanvasScripts/CanvasMainMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasMainMenu : MonoBehaviour
+{
+    private  static CanvasMainMenu instance;
+    public Canvas CanvasPlay;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void B_OnHandlePlayButton()
+    {
+        CanvasPlay.gameObject.SetActive(false);
+        PlayerMovement.GetInstance().SetIsStarted(true);
+        //FreeLookCamController.GetInstance().SetStartCam(true);
+    }
+    public static CanvasMainMenu GetInstance()
+    {
+        return instance == null ? instance = new CanvasMainMenu() : instance;
+    }
+}
