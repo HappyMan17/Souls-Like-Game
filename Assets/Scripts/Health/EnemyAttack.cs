53cc772fb822d6cb217fd915a0d3248a6eceedfa
Sebastian Grisales <sebasgrisales16@gmail.com>
2024-06-04 17:13:17 -0500

    Partial commit, logic to manage damage.

--- Assets/Scripts/Health/EnemyAttack.cs
+++ Assets/Scripts/Health/EnemyAttack.cs
@@ -7,12 +7,23 @@ public class EnemyAttack : MonoBehaviour
     [SerializeField]
     private float _damageAmount;
 
+    [SerializeField]
+    private HealthController healthController;
+
+    [SerializeField]
+    private string targetName;
+
+    [SerializeField]
+    private Animator animator;
+
     private void OnCollisionEnter(Collision collision)
     {
-
-        if (collision.gameObject.GetComponent<PlayerMovement>())
+        // if (collision.gameObject.GetComponent<PlayerMovement>())
+        //if (targetName == collision.collider.name && animator.GetBool("attack"))
+        if (targetName == collision.collider.name)
         {
-            var healthController = collision.gameObject.GetComponent<HealthController>();
+            Debug.Log(collision.collider.name);
+            //var healthController = collision.gameObject.GetComponent<HealthController>();
             Debug.Log("" + _damageAmount);
             healthController.TakeDamage(_damageAmount);
         }
