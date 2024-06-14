99b66b0c96e9bd2969fb670b34178f5eda3cbfd0
Sebastian Grisales <sebasgrisales16@gmail.com>
2024-06-14 16:41:48 -0500

    feat: New final boss added and boos door open with key using triggers.

---
+++ Assets/Scripts/Animations/Door/TriggerDoorController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoorController : MonoBehaviour
+{
+    [SerializeField] private Animator myDoor;
+
+    [SerializeField] private bool openTrigger = false;
+    [SerializeField] private bool closeTrigger = false;
+
+    [SerializeField] private PlayerInventory playerInventory;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "PaladinPlayer")
+        {
+            if (openTrigger || playerInventory.KeysCollected > 1)
+            {
+                myDoor.Play("DoorOpen", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+            else if (closeTrigger)
+            {
+                myDoor.Play("DoorClose", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+}
