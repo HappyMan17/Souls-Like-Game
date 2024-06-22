cbe3abf06332bf52036eb393f73cc38055a8d7de
Sebastian Grisales <sebasgrisales16@gmail.com>
2024-06-22 00:14:38 -0500

    feat: button to end game in the pause screen.

--- Assets/Scripts/CanvasScripts/EndGame.cs
+++ Assets/Scripts/CanvasScripts/EndGame.cs
@@ -7,8 +7,5 @@ public class EndGame : MonoBehaviour
     public void B_OnHandleEndGame()
     {
         Application.Quit();
-        // CanvasPlay.gameObject.SetActive(false);
-        // PauseController.GetInstance().SetIsPaused(false);
-        // FreeLookCamController.GetInstance().SetStartCam(true);
     }
 }
